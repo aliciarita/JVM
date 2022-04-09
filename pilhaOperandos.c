51ca15a32496d93d8482d9cb30cfda3b7822a4fd
MrJamesX37 <47198738+MrJamesX37@users.noreply.github.com>
2022-04-09 18:35:27 +0000

    Update pilhaOperandos.c

--- pilhaOperandos.c
+++ pilhaOperandos.c
@@ -1,16 +1,30 @@
+/**
+ * @file pilhaOperandos.c
+ * @brief Arquivo de implementacao das funcoes utilizadas pela Pilha de Operandos
+ */
+
 #include "pilhaOperandos.h"
 
+/**
+ * @brief Cria uma nova Pilha de Operandos
+ */
 pilha_operandos* CriaPilhaOperandos(){
 	pilha_operandos *pilha = malloc(sizeof(pilha_operandos));
 	pilha->topo = NULL;
 	return (pilha);
 }
 
+/**
+ * @brief Insere um novo operando na Pilha de Operandos
+ */
 pilha_operandos* PushOperandos(pilha_operandos *pilha, u4 operando, void *referencia, u1 tipo_operando){
 	pilha->topo = InsereInicioOperandos(pilha->topo,operando,referencia,tipo_operando);
 	return (pilha);
 }
 
+/**
+ * @brief Remove o operando do topo da Pilha de Operandos
+ */
 pilha_operandos* PopOperandos(pilha_operandos *pilha){
 	pilha_operandos *aux = CriaPilhaOperandos();
 	aux = PushOperandos(aux, pilha->topo->operando,pilha->topo->referencia, pilha->topo->tipo_operando);
@@ -18,6 +32,9 @@ pilha_operandos* PopOperandos(pilha_operandos *pilha){
 	return (aux);
 }
 
+/**
+ * @brief Informa se a Pilha de Operandos esta vazia
+ */
 bool pilhaVazia (pilha_operandos * pilha){
 	if (pilha->topo == NULL)
 		return true;
@@ -25,7 +42,9 @@ bool pilhaVazia (pilha_operandos * pilha){
 		return false;
 }
 
-
+/**
+ * @brief Informa se a Pilha de Operandos esta vazia
+ */
 bool printVazio (pilha_operandos * pilha) {
 	int contador = 0;
 	u1 tipoOpAux;
@@ -39,10 +58,16 @@ bool printVazio (pilha_operandos * pilha) {
 		return false;
 }
 
+/**
+ * @brief Acessa o topo da Pilha de Operandos
+ */
 pilha_operandos* TopoOperandos(pilha_operandos *pilha){
 	return (pilha);
 }
 
+/**
+ * @brief Imprime na tela o conteudo da Pilha de Operandos
+ */
 void ImprimePilhaOperandos(pilha_operandos *pilha){
 	ImprimeListaOperandos(pilha->topo);
 }
