a1103f275aad3fc39e527395c0764410f8f67d1e
EdiltonCostaAlvee <71397381+EdiltonCostaAlvee@users.noreply.github.com>
2022-04-10 16:21:49 -0300

    Update listaOperandos.h

--- listaOperandos.h
+++ listaOperandos.h
@@ -1,3 +1,9 @@
+
+/* *
+* @file lista_operandos.h
+@brief Estrutura e * cabeçalho da implementação da pilha de operações
+ */
+
 #ifndef LISTA_OPERANDOS_H
 #define LISTA_OPERANDOS_H
 
@@ -26,6 +32,15 @@ typedef struct lista_operandos{
 	struct lista_operandos *prox;  
 	struct lista_operandos *ant; 	
 }lista_operandos;
+
+/* *
+* @struct lista_operandos
+*
+* @brief Estrutura de dados da lista de operações.
+* @consulte https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-2.html#jvms-2.6.2
+*
+ */
+
 
 enum tipos_operandos{
 	BOOLEAN_OP = 1,
@@ -74,3 +89,36 @@ lista_operandos* InserePosicaoOperandos(lista_operandos *lista, i4 operando, u1 
 lista_operandos* RemovePosicaoOperandos(lista_operandos *lista, int posicao);
 
 #endif
+
+
+/* *
+* @brief Inicializa uma lista de operandos
+* @details A função cria uma lista de operadores sem valor e retorna o ponteiro para ela.
+* @return Lista de operações com valor nulo
+ */
+lista_operandos* CriarLista_operandos ();
+/* *
+* @brief Insere um operando (valor ou referência) no início da lista de operandos.
+* @details O tipo do operando é checado para que seja feita a inserção correta, e então
+* é alocado um espaço na memória correspondente, atribuindo ao operando enviado.
+* Apenas um tipo de operação deve ser enviado nesta função.
+*
+* @param lis Ponteiro para a lista de operandos
+* @param operando Operando de valor direto a ser inserido na lista
+* @param Operando de referência a ser inserido na lista
+* @param tipo_operando Definição do tipo de operação a ser inserido
+* @return Ponteiro para a lista de operandos após a inserção do novo operando
+ */
+lista_operandos* InserirInicio_operandos (lista_operandos *lis, i4 operando, void *referencia, u1 tipo_operando);
+/* *
+* @brief Insere um operando (valor ou referência) no final da lista de operandos.
+* @details O tipo do operando é checado para que seja feita a inserção correta, e então
+* é alocado um espaço na memória correspondente, atribuindo ao operando enviado.
+* Apenas um tipo de operação deve ser enviado nesta função.
+*
+* @param lis Ponteiro para a lista de operandos
+* @param operando Operando de valor direto a ser inserido na lista
+* @param Operando de referência a ser inserido na lista
+* @param tipo_operando Definição do tipo de operação a ser inserido
+* @return Ponteiro para a lista de operandos após a inserção do novo operando
+ */
